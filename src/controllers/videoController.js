7182010580010462f0f13b26e9aa205d8ad9d80c
flowersnakes <32151015+flowersnakes@users.noreply.github.com>
2022-07-12 09:35:22 +0900

    #6.6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -44,3 +44,19 @@ export const postEdit = (req, res) => {
   videos[id].title = title;
   return res.redirect(`/videos/${id}`);
 };
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload video" });
+};
+export const postUpload = (req, res) => {
+  //const { title } = req.body; 또는 const title = req.body.title;
+  const newVideo = {
+    title: req.body.title,
+    rating: 0,
+    comments: 0,
+    createAt: "just now",
+    views: 0,
+    id: videos.length,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
