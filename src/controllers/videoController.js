1fa618d571ddf9235526402725136ee12ac23ee0
flowersnakes <32151015+flowersnakes@users.noreply.github.com>
2022-07-18 18:55:39 +0900

    #6.20

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -13,22 +13,35 @@ export const home = async (req, res) => {
   const videos = await Video.find({});
   return res.render("home", { pageTitle: "Home", videos });
 };
-export const watch = (req, res) => {
+
+export const watch = async (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("watch", { pageTitle: video.title, video: video });
 };
-export const getEdit = (req, res) => {
+
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found." });
+  }
+  return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body; //또는 const title = req.body.title;
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload video" });
 };
+
 export const postUpload = async (req, res) => {
   //const { title } = req.body; 또는 const title = req.body.title;
   const { title, description, hashtags } = req.body;
