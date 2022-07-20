a6815b4849ab2ec19f2c14f33f73e5268e3c8f82
flowersnakes <32151015+flowersnakes@users.noreply.github.com>
2022-07-21 08:36:19 +0900

    #7.4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,23 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("Join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
   console.log(req.body);
-  const { name, username, email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const exists = await User.exists({ $or: [{ username }, { email }] });
+  const pageTitle = "Join";
+  if (password !== password2) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Password confirmation does not match.",
+    });
+  }
+
+  if (exists) {
+    return res.render("join", {
+      pageTitle: "Join",
+      errorMessage: "This username/email is already taken.",
+    });
+  }
+
   await User.create({
     name,
     username,
