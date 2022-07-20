804c660ebaad146c89ae572a07e17602a109da6b
flowersnakes <32151015+flowersnakes@users.noreply.github.com>
2022-07-20 22:23:27 +0900

    #6.28

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -10,7 +10,7 @@ Video.find({}, (error, videos) => {
 */
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -42,9 +42,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hashtags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -61,7 +59,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       //createdAt: Date.now(),
-      hashtags: hashtags,
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
@@ -70,4 +68,21 @@ export const postUpload = async (req, res) => {
       errorMessage: error._message,
     });
   }
+};
+
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: { $regex: new RegExp(`${keyword}`, "i") }, //^:Start,$:End
+    });
+  }
+  return res.render("search", { pageTitle: "Search", videos });
 };
