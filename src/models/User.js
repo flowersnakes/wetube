6239c7f32069f99cf460caf571c5ce9833391382
flowersnakes <32151015+flowersnakes@users.noreply.github.com>
2022-07-27 14:41:33 +0900

    #7.5

--- src/models/User.js
+++ src/models/User.js
@@ -17,3 +17,11 @@ userSchema.pre("save", async function () {
 
 const User = mongoose.model("User", userSchema);
 export default User;
+
+/* 데이터 베이스 명령 관련.
+show dbs => 모든 데이터베이스 나열.
+use xxx => xxx 데이터 베이스 사용.
+show collections => 데이터베이스 테이블 나열.
+db.xxxx.find() => xxxx 테이블의 모든 값을 나열.
+db.xxxx.remove({}) => 해당 테이블의 모든 데이터블 삭제.
+*/
