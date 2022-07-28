b82156bf98f0079c60f52191094ccea9527bfcba
flowersnakes <flowersnakes@nate.com>
2022-07-28 23:08:16 +0900

    #7.12

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -15,7 +16,16 @@ app.set("views", process.cwd() + "/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true })); //html에서  form을 사용하기 위해 추가
 
-app.use(session({ secret: "Hello!", resave: true, saveUninitialized: true }));
+app.use(
+  session({
+    secret: "Hello!",
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      mongoUrl: "mongodb://127.0.0.1:27017/wetube",
+    }),
+  })
+);
 
 /* 모든 세션을 확인 할때 사용
 app.use((req, res, next) => {
