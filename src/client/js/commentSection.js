5fb6488223b9139e8490669a7d45c841c1d97449
flowersnakes <flowersnakes@nate.com>
2022-08-19 00:04:25 +0900

    Update commentSection.js

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -6,6 +6,7 @@ const btn = form.querySelector("button");
 const handleSubmit = (event) => {
   event.preventDefault();
   const text = textarea.value;
+  const video = videoContainer.dataset.id;
 };
 
 form.addEventListener("submit", handleSubmit);
